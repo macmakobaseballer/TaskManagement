2d78a1285369288854d2eb96c4f071b8d4b10de4
ne-uter <defendo_habeo@yahoo.co.jp>
2021-10-12 22:26:04 +0900

    [fix] #14 DB0001 schema.sqlの作成
    DB指定の削除と予約語を大文字表記に変更

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,8 @@
-use task_management;
-
-create table if not exists task_management.tasks(
-	task_id int auto_increment,
-	completed_flag boolean not null default 0,
-	task_content varchar(100) not null,
-	task_note varchar(254),
-	task_deadline date,
-	primary key(task_id)
-)default charset=utf8;
+CREATE TABLE IF NOT EXISTS tasks(
+	task_id INT AUTO_INCREMENT,
+	completed_flag BOOLEAN NOT NULL DEFAULT 0,
+	task_content VARCHAR(100) NOT NULL,
+	task_note VARCHAR(254),
+	task_deadline DATE,
+	PRIMARY KEY(task_id)
+)DEFAULT CHARSET=utf8;

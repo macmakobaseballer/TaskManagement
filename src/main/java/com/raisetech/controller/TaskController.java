24965357371dde8c49a0267ce3a3001d966b660e
shiryu1027 <asmttmcz.1027@outlook.jp>
2021-10-13 15:16:52 +0900

    [add] #2 VI0002 タスク一覧表示画面の作成
    TaskControllerのGET処理、index.htmlを作成
    ※serviceクラスのメソッドは別チケットで作成するため、このままではエラーが起こる =>
    serviceクラスを作成(#6、#7)してからPull Requestを出す

--- src/main/java/com/raisetech/controller/TaskController.java
+++ src/main/java/com/raisetech/controller/TaskController.java
@@ -1,5 +1,27 @@
 package com.raisetech.controller;
 
-public class TaskController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.raisetech.service.TaskService;
+
+@Controller
+@RequestMapping("/task")
+public class TaskController {
+	
+	@Autowired
+	TaskService taskService;
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		
+		model.addAttribute("tasks", taskService.getTasks());
+		model.addAttribute("finTasks", taskService.getFinTasks());
+		
+		return "task/index";
+	}
+	
 }

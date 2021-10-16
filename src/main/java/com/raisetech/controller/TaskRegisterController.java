bbfb8fa2c7c5adcda408e74c0707474b8ae08f57
MACBOOK_MAKO <>
2021-10-17 00:03:17 +0900

    [add] #3 VI0003 タスク新規登録画面
    ControllerクラスのGetMapping・HTMLの作成

--- src/main/java/com/raisetech/controller/TaskRegisterController.java
+++ src/main/java/com/raisetech/controller/TaskRegisterController.java
@@ -1,5 +1,23 @@
 package com.raisetech.controller;
 
+import com.raisetech.service.TaskService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/task")
 public class TaskRegisterController {
+
+    @Autowired
+    TaskService taskService;
+
+    @GetMapping("/register")
+    public String register(){
+
+        return "task/register";
+    }
 
 }

c35861cf55bbb00dfe12988e0597eb04fea71b8f
ne-uter <defendo_habeo@yahoo.co.jp>
2021-10-21 22:15:12 +0900

    [add]#4 VI0004 タスクの詳細表示画面
    タスク詳細画面作成

--- src/main/java/com/raisetech/controller/TaskDetailController.java
+++ src/main/java/com/raisetech/controller/TaskDetailController.java
@@ -1,5 +1,65 @@
 package com.raisetech.controller;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.raisetech.entity.Task;
+import com.raisetech.form.DetailForm;
+import com.raisetech.service.TaskService;
+
+@Controller
+@RequestMapping("/task")
 public class TaskDetailController {
 
+	@Autowired
+	TaskService taskService;
+	
+	@Autowired
+	ModelMapper modelMapper;
+	
+	//詳細画面表示
+	@GetMapping("/detail/id={id}")
+	public String getDetail(@PathVariable int id,Model model,@ModelAttribute DetailForm form) {
+		model.addAttribute("detailForm", taskService.getTaskDetails(id));
+		
+		return "task/detail";
+	}
+	
+	//タスク詳細の更新処理（完了フラグ変更なし）
+	@PostMapping(value = "/detail/id={id}",params = "update")
+	public String postDetail(@PathVariable int id,Model model,@ModelAttribute DetailForm form){
+		//画面から受け取ったformの内容をtaskに渡す
+		Task task = modelMapper.map(form,Task.class);
+		task.setTaskId(id);
+	    //更新処理
+		taskService.updateTaskDetails(task);
+		
+		return "redirect:/task/";
+	}
+	
+	//タスク詳細の更新処理（完了フラグ変更あり）
+	@PostMapping(value = "/detail/id={id}",params = "complete")
+	public String postTaskCompleted(@PathVariable int id,Model model,@ModelAttribute DetailForm form){
+		//画面から受け取ったformの内容をtaskに渡す
+		Task task = modelMapper.map(form,Task.class);
+		task.setTaskId(id);
+	    //更新処理
+		taskService.updateTaskAllDetails(task);
+		
+		return "redirect:/task/";
+	}
+	//タスクの削除
+	@PostMapping(value = "/detail/id={id}",params = "delete")
+	public String postDelete(@PathVariable int id,Model model,@ModelAttribute DetailForm form) {
+		 taskService.deleteTask(id);
+		
+		return "redirect:/task/";
+	}
 }

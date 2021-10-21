64b05fb9c59ebf70cb41705142c300a9e9daa5cb
shiryu1027 <asmttmcz.1027@outlook.jp>
2021-10-21 15:59:43 +0900

    [add] #2 VI0002 タスク一覧表示画面
    完了フラグを変更するPost処理を追加

--- src/main/java/com/raisetech/controller/TaskController.java
+++ src/main/java/com/raisetech/controller/TaskController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.raisetech.service.TaskService;
@@ -22,6 +24,24 @@ public class TaskController {
 		model.addAttribute("completedTasks", taskService.getCompletedTasks());
 		
 		return "task/index";
+	}
+	
+	// 完了フラグ変更(false -> true)
+	@PostMapping("/complete/id={id}")
+	public String taskComplete(@PathVariable("id") int id) {
+		
+		taskService.updateCompletedFlagToTrue(id);
+		
+		return "redirect:/task/";
+	}
+	
+	// 完了フラグ変更(true -> false)
+	@PostMapping("/incomplete/id={id}")
+	public String taskIncomplete(@PathVariable("id") int id) {
+		
+		taskService.updateCompletedFlagToFalse(id);
+		
+		return "redirect:/task/";
 	}
 	
 }

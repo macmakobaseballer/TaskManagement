5b5e534842de6e10bf7b91fe1e309f1062ff15ad
MACBOOK_MAKO <>
2021-10-23 23:06:33 +0900

    [add] #15 VI0005 Validation処理
    register.htmlのバリデーション実装

--- src/main/java/com/raisetech/form/RegisterForm.java
+++ src/main/java/com/raisetech/form/RegisterForm.java
@@ -2,18 +2,25 @@ package com.raisetech.form;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
+
 
 import lombok.Data;
 
 @Data
 public class RegisterForm {
 
+    @Length(min=1,max=100)
+    @NotNull
     private String taskContent;
 
     @DateTimeFormat(pattern="yyyy/MM/dd")
     private LocalDate taskDeadline;
 
+    @Length(max=200)
     private String taskNote;
 
 }

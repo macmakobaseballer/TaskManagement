0f71f8ec554d22cd8217a144e899570c3ead336b
MACBOOK_MAKO <>
2021-11-01 21:22:25 +0900

    [add] #18 C0001-G11 TopControllerのテスト作成
    GETメソッドのテスト実装

--- src/test/java/com/raisetech/controller/TopControllerTest.java
+++ src/test/java/com/raisetech/controller/TopControllerTest.java
@@ -1,19 +1,17 @@
 package com.raisetech.controller;
 
-import com.raisetech.service.TaskService;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.raisetech.service.TaskService;
 
 @SpringBootTest
 // @TestPropertySource(locations = "classpath: test.properties")
@@ -21,8 +19,8 @@ public class TopControllerTest {
 
     private MockMvc mockMvc;
 
-    // @MockBean
-    // private TaskService service;
+    @Autowired
+    private TaskService service;
 
     @Autowired
     private TopController target;
@@ -38,7 +36,17 @@ public class TopControllerTest {
 
     @Test
     public void トップ画面の表示() throws Exception {
-        mockMvc.perform(get("/task")).andExpect(status().isOk()).andExpect(view().name("task/index"));
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("task/index"));
     }
+
+    //modelに値を渡せることのテストってmockitoとかで振る舞い定義する？？それともエラーがないことだけ検証する？
+    @Test
+    public void タスクの一覧をmodelに格納する際にエラーがない() throws Exception {
+        mockMvc.perform(get("/")).andExpect(model().hasNoErrors());
+    }
+
+    
+
+
 
 }
